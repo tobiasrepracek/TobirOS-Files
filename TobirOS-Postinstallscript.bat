269a203b0ad98a4b391c125e6f2b3f82381cd506
tobiasrepracek <80835891+tobiasrepracek@users.noreply.github.com>
2023-05-21 20:01:18 +0200

    Add files via upload

---
+++ TobirOS-Postinstallscript.bat
@@ -0,0 +1,9 @@
+@echo off
+color 0a
+title TobirOS | Setup
+
+echo Installing Tobir's startup script...
+cd "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+curl -s https://raw.githubusercontent.com/tobiasrepracek/TobirOS-Files/main/startup.bat > startup.bat
+
+echo Installed startup script...
